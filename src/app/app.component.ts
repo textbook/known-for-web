ceb1e85be3c1a5fc6998fa89fa2df1b716fc5472
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-08-28 21:17:06 +0100

    Implement styling adjustments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'Known For';
