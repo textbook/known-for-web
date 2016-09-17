ea1143326c73ad4dd84eea6db353021e6fe156bf
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2016-09-17 16:13:13 +0100

    Upgrade to latest Angular CLI and Angular 2.0.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('App: KnownForWeb', () => {
@@ -6,6 +8,7 @@ describe('App: KnownForWeb', () => {
 
   beforeEach(done => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     });
 
